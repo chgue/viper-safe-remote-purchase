9c7f5f27d9599761d0e3b90808dd04c095af7aa3
chgue <chgue@users.noreply.github.com>
2017-10-08 00:37:50 +0200

    Multiple changes
    1. Removed assert (msg.value % 2) == 0 since viper supports decimals
    2. Locked state is determined by the contract balance
    3. Comments explaining transaction added

--- safe-remote-purchase.v.py
+++ safe-remote-purchase.v.py
@@ -1,33 +1,38 @@
-#Safe Remote Purchase (https://github.com/ethereum/solidity/blob/develop/docs/solidity-by-example.rst) ported to viper
-#Value of the item
-value: public(wei_value)
+#Safe Remote Purchase (https://github.com/ethereum/solidity/blob/develop/docs/solidity-by-example.rst) ported to viper and optimized
+
+#Rundown of the transaction:
+#1. Seller posts item for sale and posts safety deposit of double the item value. Balance is 2*value.
+#(1.1. Seller can reclaim deposit and close the sale as long as nothing was purchased.)
+#2. Buyer purchases item (value) plus posts an additional safety deposit (Item value). Balance is 4*value
+#3. Seller ships item
+#4. Buyer confirms receiving the item. Buyer's deposit (value) is returned. Seller's deposit (2*value) + items value is returned. Balance is 0.
+
+value: public(wei_value) #Value of the item
 seller: public(address)
 buyer: public(address)
-state: num # 0 = created, 1 = locked, 2 = finished/inactive
-#Seller initializes the purchase by depositing 2*value in the contract
+
+@constant
+def unlocked() -> bool: #Is a refund possible for the seller?
+    return (self.balance == self.value*2)
+    
 @payable
 def __init__():
-    #Assert that the value is a multiple of 2
-    assert (msg.value % 2) == 0
-    self.value = msg.value / 2
+    self.value = msg.value / 2 #Seller initializes contract by posting a safety deposit of 2*value of the item up for sale
     self.seller = msg.sender
-    self.state = 0 #Created
+    
 def abort():
-    assert self.state == 0 #Is the contract still refundable
+    assert self.unlocked() == true #Is the contract still refundable
     assert msg.sender == self.seller #Only seller can refund his deposit before any buyer purchases the item
-    self.state = 2 #Possible unnecessary because of selfdestruct?
-    selfdestruct(self.seller) #Refunds seller
+    selfdestruct(self.seller) #Refunds seller, deletes contract
 
 @payable
 def purchase():
-    assert self.state == 0 #Contract still "created" which means item still up for sale?
+    assert self.unlocked() == true #Contract still open (item still up for sale)?
     assert msg.value == (2*self.value) #Is the deposit of correct value?
     self.buyer = msg.sender
-    self.state = 1 #Item is sold, seller cannot refund anymore
+    
 def recieved():
-    assert self.state == 1 #Is the item already purchased and pending confirmation of buyer
+    assert self.unlocked() == false #Is the item already purchased and pending confirmation of buyer
     assert msg.sender == self.buyer 
-    self.state = 2 #Contract finished. Unnecessary because of selfdestruct?
-    send(self.buyer, self.value) #Return deposit to buyer
-    selfdestruct(self.seller)
-
+    send(self.buyer, self.value) #Return deposit (=value) to buyer
+    selfdestruct(self.seller) #Returns deposit (=2*value) and the purchase price (=value)
